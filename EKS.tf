2a4a98962cdf9085522702171d907c738e3c14d6
kfrum526 <kfrum4@gmail.com>
2022-03-15 20:29:54 -0400

    vpc and eks update

--- EKS.tf
+++ EKS.tf
@@ -8,7 +8,8 @@ resource "aws_iam_role" "eksiamtest" {
         {
             "Effect": "Allow",
             "Principal": {
-                "Service": "eks.amazonaws.com"
+                "Service": "eks.amazonaws.com",
+                "Service": "ec2.amazonaws.com"
             },
             "Action": "sts:AssumeRole"
         }
@@ -18,11 +19,48 @@ POLICY
 }
 
 
+resource "aws_iam_role_policy_attachment" "eksiamtest-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eksiamtest.name
+}
+resource "aws_iam_role_policy_attachment" "eksiamtest-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role = aws_iam_role.eksiamtest.name
+}
+resource "aws_iam_role_policy_attachment" "eksiamtest-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.eksiamtest.name
+}
+resource "aws_iam_role_policy_attachment" "eksiamtest-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role = aws_iam_role.eksiamtest.name
+}
+
 resource "aws_eks_cluster" "testcluster" {
   name = "testcluster"
   role_arn = aws_iam_role.eksiamtest.arn
 
   vpc_config {
     subnet_ids = [aws_subnet.public.id, aws_subnet.private.id]
+  }
+}
+
+resource "aws_eks_node_group" "nodegrouptest" {
+  cluster_name = aws_eks_cluster.testcluster.name
+  node_group_name = "TestNodeGroup"
+  node_role_arn = aws_iam_role.eksiamtest.arn
+  subnet_ids = [aws_subnet.private.id, aws_subnet.public.id]
+
+  instance_types =["t2.micro","t2.micro"]
+  ami_type = "AL2_x86_64"
+
+  scaling_config {
+    desired_size = 1
+    max_size = 2
+    min_size = 1
+  }
+
+  update_config {
+    max_unavailable = 2
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
  vpc_id = aws_vpc.main.id
  cidr_block = "10.0.1.0/24"
+ availability_zone = "us-east-1a"
 
  tags = {
      Name = "Public Subnet"
@@ -18,6 +19,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
  vpc_id = aws_vpc.main.id
  cidr_block = "10.0.2.0/24"
+ availability_zone = "us-east-1b"
 
  tags = {
      Name = "Private Subnet"
