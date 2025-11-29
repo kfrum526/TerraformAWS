eb0bafebdf2b4894b72cebd3cd8707d82378a920
Kevin Frum <kfrum4@gmail.com>
2025-11-29 11:58:23 -0500

    fixing indentations

--- vpc.tf
+++ vpc.tf
@@ -3,31 +3,24 @@ resource "aws_vpc" "main" {
 
   tags = {
       Name = "main"
-      "kubernetes.io/cluster/${var.eks_name}" = "owned"
   }
 }
 
 resource "aws_subnet" "public" {
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.1.0/24"
- availability_zone = "us-east-1a"
- map_public_ip_on_launch = true
-
- tags = {
-     Name = "Public Subnet"
-     "kubernetes.io/cluster/${var.eks_name}" = "shared"
- } 
+  Name = "Public Subnet"
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
- vpc_id = aws_vpc.main.id
- cidr_block = "10.0.2.0/24"
- availability_zone = "us-east-1b"
- map_public_ip_on_launch = true
- tags = {
-     Name = "Private Subnet"
-     "kubernetes.io/cluster/${var.eks_name}" = "shared"
- } 
+  Name = "Private Subnet"
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
 }
 
 resource "aws_internet_gateway" "gw" {
